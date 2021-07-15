db981a89dc972a265a43c6d8d88efaaae9300403
websmallrabbit <wk_web@163.com>
2021-07-15 18:56:24 +0800

    端口

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,86 @@
 <template>
   <div id='app'>
-  <h1 class='top'>神仙保佑🙏，追虎擒龙， 风水大师，得水为上，藏风次之。左青龙右白虎，前朱雀后玄武</h1>
-  <h1 class='title'>大乐透预测</h1>
-  <div class='taCenter'>
-    <img class='xuanwu' src='./assets/xuanwu.jpeg'>
-  </div>
-  <div class='taCenter'>
-    <div>
-      <h3 class='front-area' v-for='(item, index) in frontArea' :key='index'>
-        {{item}}
-      </h3>
-    </div>
-    <div>
-      <h3 class='front-area' v-for='(item, index) in endArea' :key='index'>
-        {{item}}
-      </h3>
-    </div>
-    <div>
-      <button @click='newNum'>点击生成</button>
-      <input type='date' v-model='date' >
-    </div>
-  </div>
+    <div class='content-wrapper' ref='contentRef' @click='handleOpenDia'>
+      <div class='bg-box bg-blur'>
+        <!--      <img src='./assets/bg.jpeg'>-->
+      </div>
+      <div class='container'>
+        <h1 class='top'>神仙保佑🙏，追虎擒龙， 风水大师，得水为上，藏风次之。左青龙右白虎，前朱雀后玄武, 彩票开光，必中大奖🎉🎉🎉</h1>
+        <h1 class='title'>风水大乐透预测</h1>
+        <div class='taCenter'>
+          <img class='xuanwu' src='./assets/xuanwu.jpeg'>
+        </div>
 
-  <div class='taLeft'>
-    <div class='get-item'>
-      <span>中奖日期：</span>
-      <span class='get-area'>{{date}}</span>
-    </div>
-    <div class='get-item'>
-      <span>前区：</span>
-      <span class='get-area' v-for='(item, index) in getFrontArea' :key='index'>{{item}}</span>
-    </div>
-    <div class='get-item'>
-      <span>后区：</span>
-      <span class='get-area' v-for='(item, index) in getEndArea' :key='index'>{{item}}</span>
-    </div>
-  </div>
+        <div class='taCenter'>
+          <div>
+            <h3 class='front-area' v-for='(item, index) in frontArea' :key='index'>
+              {{item}}
+            </h3>
+          </div>
+          <div class='endArea'>
+            <h3 class='front-area' v-for='(item, index) in endArea' :key='index'>
+              {{item}}
+            </h3>
+          </div>
+          <div>
+            <button @click='newNum'>点击生成</button>
+            <input type='date' v-model='date' >
+          </div>
+        </div>
+        <div class='taLeft'>
+          <div class='get-item'>
+            <span>中奖日期：</span>
+            <span class='get-area'>{{date}}</span>
+          </div>
+          <div class='get-item'>
+            <span>前区：</span>
+            <span class='get-area' v-for='(item, index) in getFrontArea' :key='index'>{{item}}</span>
+          </div>
+          <div class='get-item'>
+            <span>后区：</span>
+            <span class='get-area' v-for='(item, index) in getEndArea' :key='index'>{{item}}</span>
+          </div>
+        </div>
 
-  <div class='taLeft'>
-    <div v-for='(items, index) in allNum' :key='index'>
-      <span>(第{{index+1}}个号码)</span>
-      前区：<span class='get-area' v-for='(item, index) in items.front' :key='index'>{{item}}</span>
-      后区：<span class='get-area' v-for='(item, index) in items.end' :key='index'>{{item}}</span>
-      中奖日期：<span>{{date}}</span>
-      <button @click='handleDet(index)'>删除</button>
+        <div class='content-box'>
+          <div class='taLeft'>
+            <div v-for='(items, index) in allNum' :key='index'>
+              <span>(第{{index+1}}个号码)</span>
+              前区：<span class='get-area' v-for='(item, index) in items.front' :key='index'>{{item}}</span>
+              后区：<span class='get-area' v-for='(item, index) in items.end' :key='index'>{{item}}</span>
+              中奖日期：<span>{{date}}</span>
+              <button @click='handleDet(index)'>删除</button>
+            </div>
+          </div>
+          <div class='taCenter'>
+            <img class='zhuque' src='./assets/zhuque.jpeg'>
+          </div>
+        </div>
+
+        <img class='baihu' src='./assets/baihu.jpeg'>
+        <img class='qinglong' src='./assets/qinglong.jpeg'>
+
+      </div>
     </div>
-  </div>
-  <div class='taCenter'>
-    <img class='zhuque' src='./assets/zhuque.jpeg'>
-  </div>
-
-  <img class='baihu' src='./assets/baihu.jpeg'>
-  <img class='qinglong' src='./assets/qinglong.jpeg'>
-
+    <el-dialog
+            title="提示"
+            :visible.sync="dialogVisible"
+            width="30%"
+            :before-close="handleClose">
+      <span>
+       <div class="demo-input-suffix">
+        请填写随机码：
+        <el-input
+                placeholder="请填写随机码"
+                v-model="randomMa">
+        </el-input>
+      </div>
+      </span>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click='getMa'>点击获取随机码</el-button>
+        <el-button @click="handleMa">确 定</el-button>
+       </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -66,7 +94,19 @@ export default {
       getFrontArea: [],
       getEndArea: [],
       date: new Date().toLocaleDateString().replace(/\//g, '-'),
-      allNum: []
+      allNum: [],
+      dialogVisible: false,
+      randomMa: ''
+    }
+  },
+  created() {
+    window.oncontextmenu=function(){return false;}
+    // 禁止任何键盘敲击事件（防止F12和shift+ctrl+i调起开发者工具）
+    window.onkeydown = window.onkeyup = window.onkeypress = function (event) {
+      if(event.code === 'F12'){
+        window.event.returnValue = false;
+        return false;
+      }
     }
   },
   methods: {
@@ -98,6 +138,29 @@ export default {
     },
     handleDet(index) {
       this.allNum.splice(index, 1)
+    },
+    handleClose() {
+
+    },
+    handleOpenDia() {
+      this.dialogVisible = true
+    },
+    getMa() {
+      window.location.href = 'https://www.xiaocifang.com/i/GOc7c9b8d86DV.html'
+    },
+    handleMa() {
+      if(this.randomMa === '392042') {
+        this.dialogVisible = false
+        this.$message({
+          message: '验证码正确，预祝您中的头彩',
+          type: 'success'
+        });
+      }else {
+        this.$message({
+          message: '您输入的验证码错误，请点击链接获取验证码',
+          type: 'warning'
+        });
+      }
     }
   }
 
@@ -105,12 +168,22 @@ export default {
 </script>
 
 <style>
+  h1, h3 {
+    margin: 0;
+    padding: 0;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+  }
 #app {
   position: relative;
+  height: 100vh;
 }
 .top {
   display: block;
   text-align: center;
+  font-size: 24px;
 }
 .title {
   text-align: center;
@@ -128,11 +201,11 @@ export default {
 
 .taLeft {
   margin-left: 200px;
-  margin-top: 30px;
+  margin-top: 10px;
 }
 
 .get-item {
-  margin-bottom: 20px;
+  margin-bottom: 10px;
   color: red;
 }
 
@@ -163,5 +236,45 @@ export default {
   top: 340px;
   width: 120px;
 }
+
+  .content-box {
+    height: 100px;
+    overflow: auto;
+    /*position: absolute;*/
+  }
+  .endArea {
+    margin: 10px auto;
+  }
+  .container {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    z-index: 10;
+  }
+  .bg-blur {
+    float: left;
+    width: 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    filter: blur(6px);
+  }
+  .bg-box {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+  }
+  .bg-box img {
+    width: 100%;
+    height: 100vh;
+  }
+  .el-input {
+    display: inline-block !important;
+    width: 200px !important;
+  }
 
 </style>
